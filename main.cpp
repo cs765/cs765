cfa29bc36d56b442fd94a6c5f803e18c371e8c76
Kushal Hada <kushaldeveloper@gmail.com>
2018-02-04 18:53:30 -0500

    fix spelling for compounded

--- main.cpp
+++ main.cpp
@@ -10,14 +10,14 @@ double future_value(double F0, double t0, double t1, double r) {
 int main() {
     double initial_value = 100.0;
     double initial_time = 0.0;
-    double future_time = 0.0;
+    double future_time = 10.0;
     double continuously_compounded_interest_rate = 10.0;
     std::cout
             << "We calculate the future value of $"
             << initial_value
             << " "
             << future_time
-            << " years from today at a continuously compunded interest rate of "
+            << " years from today at a continuously compounded interest rate of "
             << continuously_compounded_interest_rate
             << "%. Please stand by while we calculate the results. "
             << "The expected future value is: "
