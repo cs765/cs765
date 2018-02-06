766205d80dd6e0e62338a5d89e8a304e39242751
Kushal Hada <kushaldeveloper@gmail.com>
2018-02-05 22:22:45 -0500

    WIP
    this is incorrect
    
    I don't know why it is wrong

--- main.cpp
+++ main.cpp
@@ -1,19 +1,17 @@
 #include <cmath>
 #include <iostream>
 
-double get_bond_price(double face, double yield, double coupon, double number_of_coupons) {
+double get_bond_price(double face, double yield, double coupon1, double coupon2, double coupon3, double coupon4) {
     double running_total = 0;
-    for (int counter = 1; counter <= number_of_coupons; counter++) {
-        running_total += 0.5 * coupon / pow(1 + 0.5 * yield, counter);
-    }
-    running_total += (face / pow(1 + 0.5 * yield, number_of_coupons));
+    // one
+    running_total += (0.5 * coupon1) / (1 + 0.5 * yield);
+    running_total += (0.5 * coupon2) / pow(1 + 0.5 * yield, 2);
+    running_total += (0.5 * coupon3) / pow(1 + 0.5 * yield, 3);
+    running_total += (face + 0.5 * coupon4) / pow(1 + 0.5 * yield, 4);
     return running_total;
 }
 
 int main() {
-    int yields[5] = {0, 2, 4, 6, 8};
-    for (int yield : yields) {
-        std::cout << get_bond_price(100, yield / 100, 4, 4) << std::endl;
-    }
+    std::cout << get_bond_price(100, 8 / 100, 4, 4, 4, 4) << std::endl;
     return 0;
 }
