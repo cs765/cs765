a9bc0046c58187c9861fac675f0241b798e4028e
Kushal Hada <kushaldeveloper@gmail.com>
2018-02-07 06:10:46 -0500

    use doubles not ints

--- main.cpp
+++ main.cpp
@@ -2,16 +2,21 @@
 #include <iostream>
 
 double get_bond_price(double face, double yield, double coupon1, double coupon2, double coupon3, double coupon4) {
-    double running_total = 0;
+    double running_total = 0.0;
     // one
     running_total += (0.5 * coupon1) / (1 + 0.5 * yield);
+    std::cout << running_total << std::endl;
     running_total += (0.5 * coupon2) / pow(1 + 0.5 * yield, 2);
+    std::cout << running_total << std::endl;
     running_total += (0.5 * coupon3) / pow(1 + 0.5 * yield, 3);
+    std::cout << running_total << std::endl;
     running_total += (face + 0.5 * coupon4) / pow(1 + 0.5 * yield, 4);
+    std::cout << running_total << std::endl;
+
     return running_total;
 }
 
 int main() {
-    std::cout << get_bond_price(100, 8 / 100, 4, 4, 4, 4) << std::endl;
+    std::cout << get_bond_price(100.0, 8.0 / 100.0, 4.0, 4.0, 4.0, 4.0) << std::endl;
     return 0;
 }
